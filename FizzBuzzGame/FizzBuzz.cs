72348897801746ab88bded729c197ba70cf60556
bajonator <bajo123@wp.pl>
2023-04-16 23:34:35 +0200

    Add project files.

---
+++ FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    public class FizzBuzz
+    {
+        public string ShowResult()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int fizzbuzz))
+            {
+                Console.WriteLine("Podaj prawidłową wartość.");
+                return "";
+            }
+            if (fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0)
+                return "FizzBuzz\n";
+            if (fizzbuzz % 5 == 0)
+                return "Buzz\n";
+            if (fizzbuzz % 3 == 0)
+                return "Fizz\n";
+            return fizzbuzz.ToString(); 
+        }
+    }
+}
---
+++ FizzBuzzGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz();
+
+                Console.WriteLine("Wpisz Liczbę:");
+                var userNumber = fizzBuzz.ShowResult();
+                Console.WriteLine(userNumber.ToString());
+            }
+        }
+    }
+}
